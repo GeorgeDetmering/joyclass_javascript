906563be6c87291a3eb2d79d7cf7e463b39d4277
George Detmering <georgedetmering@gmail.com>
2023-08-19 22:42:56 -0300

    Create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,17 @@
+// ********** CADASTRO DE USUÁRIO **********
+
+console.log("********** CADASTRO DE USUÁRIO **********");
+
+const nome = "George Detmering";
+console.log("Nome:", nome);
+
+let salario = 5800.00;
+console.log("Salário $:", salario);
+
+let idade = 46;
+console.log("Idade:", idade);
+
+let diploma = true;
+console.log("Diploma:", diploma ? "Sim" : "Não");
+    
+console.log("*****************************************")
